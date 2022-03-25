6a8a4bac7a2df23bfeffd2e820c62778f212197a
Weydson Marinho <weydsonmarinho@gmail.com>
2022-03-25 16:57:05 -0300

    add: Home styling and content - fetching content from Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
+
+import Header from '../components/Header';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,13 +33,79 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ posts }) {
+  return (
+    <div className={`${commonStyles.container} ${styles.homeContainer}`}>
+      <div>
+        <Header />
+      </div>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main>
+        {posts.map(post => {
+          const {
+            slug,
+            first_publication_date,
+            data: { author, title, subtitle },
+          } = post;
+          console.log(author, title);
+          return (
+            <article className={styles.article}>
+              <Link href={`/posts/${slug}`} key={slug}>
+                <a>
+                  <div>
+                    <h2 className={commonStyles.title}>{title}</h2>
+                    <p className={commonStyles.subtitle}>{subtitle}</p>
+                  </div>
+                  <div className={commonStyles.info}>
+                    <div>
+                      <FiCalendar color="#BBBBBB" />
+                      <span>
+                        {format(
+                          new Date(first_publication_date),
+                          'd MMM yyyy',
+                          {
+                            locale: ptBR,
+                          }
+                        )}
+                      </span>
+                    </div>
+                    <div>
+                      <FiUser color="#BBBBBB" />
+                      <span>{author}</span>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+            </article>
+          );
+        })}
 
-//   // TODO
-// };
+        <div className={styles.loadMore}>
+          <button>Carregar mais posts</button>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'post'),
+  ]);
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        author: post.data.author,
+        subtitle: post.data.subtitle,
+        title: post.data.title,
+      },
+    };
+  });
+  console.log(new Date());
+
+  return { props: { posts } };
+};
