e327399b5fc3ba8c00a6d038c83c3431865e227e
Weydson Marinho <weydsonmarinho@gmail.com>
2022-03-29 14:37:55 -0300

    [slug].tsx, Fetching post and slugs from Prismic

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,8 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { Container } from 'next/app';
+import { RichText } from 'prismic-dom';
 
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -26,20 +29,52 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return <div>hi</div>;
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'post'),
+  ]);
 
-//   // TODO
-// };
+  const slugs = posts.results.map(post => post.uid);
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths: slugs.map(slug => {
+      return { params: { slug } };
+    }),
+    fallback: true,
+  };
+};
 
-//   // TODO
-// };
+export const getStaticProps = async context => {
+  const { slug } = context.params;
+  console.log(slug);
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const post = {
+    first_publication_date: response.first_publication_date,
+    data: response.data,
+    title: response.data.title,
+    banner: {
+      url: response.data.banner?.url,
+    },
+    author: response.data.author,
+    content: response.data.content.map(content => {
+      return {
+        heading: content.heading,
+        body: content.body.map(body => {
+          return {
+            text: body.text,
+          };
+        }),
+      };
+    }),
+  };
+
+  return { props: { post } };
+};
